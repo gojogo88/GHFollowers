0aafd426707b5ad65da5aacfd6b9a07900d9757d
Jonathan Go <gojonathan.dev@gmail.com>
2019-12-29 11:58:50 +0800

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Jonathan Go on 12/29/19.
+//  Copyright © 2019 SonnerStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+

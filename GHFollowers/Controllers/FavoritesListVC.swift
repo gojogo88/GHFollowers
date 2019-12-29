45df3a66d2ca3c5e4beee39544fa3af9bc5d936b
Jonathan Go <gojonathan.dev@gmail.com>
2019-12-29 13:10:40 +0800

    created tabbar, navcontrollers and searched login screen

--- GHFollowers/Controllers/FavoritesListVC.swift
+++ GHFollowers/Controllers/FavoritesListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListVC.swift
 //  GHFollowers
 //
 //  Created by Jonathan Go on 12/29/19.
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesListVC: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
 
+    view.backgroundColor = .systemBlue
+    
+  }
+    
+
+    
 
 }
-
---
+++ GHFollowers/Resource/GFButton.swift
@@ -0,0 +1,38 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Jonathan Go on 12/29/19.
+//  Copyright © 2019 SonnerStudio. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    configure()
+  }
+  
+  init(backgroundColor: UIColor, title: String) {
+    super.init(frame: .zero)
+    
+    self.backgroundColor = backgroundColor
+    self.setTitle(title, for: .normal)
+    configure()
+  }
+  
+  private func configure() {
+    layer.cornerRadius    = 10
+    titleLabel?.textColor = .white
+    titleLabel?.font      = UIFont.preferredFont(forTextStyle: .headline)
+    translatesAutoresizingMaskIntoConstraints = false
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+}
